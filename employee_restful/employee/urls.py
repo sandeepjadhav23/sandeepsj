5a8929e8a59e7d29c2073f6a4dc75ab9bad7ba32
sandeepsj2323 <sandeepsj2323@gmail.com>
2023-03-25 06:38:52 +0530

    second commit

--- employee_restful/employee/urls.py
+++ employee_restful/employee/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from employee import views
 
 urlpatterns = [
-    path('employee/', views.EmployeeListCreate.as_view()),
+    path('employees/', views.EmployeeListCreate.as_view()),
     path('employees/<int:pk>/', views.EmployeeRetrieveUpdateDelete.as_view()),
 ]
